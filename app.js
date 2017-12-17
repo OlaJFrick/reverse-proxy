1a0bf1bb38bcf2736a6b7bfd4b9c856ef851b0d4
Ola Frick <info@culturehero.org>
2017-12-17 22:55:19 +0100

    Test

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// Requires
-const http = require('http');
-const https = require('https');
-const httpProxy = require('http-proxy');
-const tls = require('tls');
-const fs = require('fs');
-const path = require('path');
-const exec = require('child_process').exec;
+// requires 
+const http = require("http");
+const https = require("https");
+const httpProxy = require("http-proxy");
+const tls = require("tls");
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").exec;
 
-// Read our routes
-const routes = require('./routing.json');
+// read our routes
+const routes = require("./routing.json");
 
-// Read all certs from certbot into an object
+// read all certs from certbot into an object
 let certs = readCerts("/etc/letsencrypt/live");
 
 // Create a new reverse proxy
@@ -18,137 +18,129 @@ const proxy = httpProxy.createProxyServer();
 
 // Handle proxy errors - thus not breaking the whole
 // reverse-proxy app if an app doesn't answer
-proxy.on('error',function(e){
-  console.log('Proxy error', Date.now(), e);
-})
+proxy.on("error", function(e) {
+  console.log("Proxy error: ", e);
+});
 
-// Create a new unencrypted webserver
-// with the purpose to answer certbot challenges
-// and redirect all other traffic to https
-http.createServer((req,res)=>{
+// Create a new unencrypted webserver for certbot challenges
+// and redirects to https
+http
+  .createServer((req, res) => {
+    const urlParts = req.url.split("/");
 
-  let urlParts = req.url.split('/');
-
-  if(urlParts[1] == '.well-known'){
-    // using certbot-helper on port 5000
-    proxy.web(req,res,{target:'http://127.0.0.1:5000'});
-  }
-  else {
-    // redirect to https
-    let url = 'https://' + req.headers.host + req.url;
-    res.writeHead(301, {'Location': url});
-    res.end();
-  }
-
-}).listen(80);
+    if (urlParts[1] == ".well-known") {
+      // using certbot-helper on port 5000
+      proxy.web(req, res, { target: "http://127.0.0.1:5000" });
+    } else {
+      // redirect to https
+      let url = "https://" + req.headers.host + req.url;
+      res.writeHead(301, { Location: url });
+      res.end();
+    }
+  })
+  .listen(80);
 
 // Create a new secure webserver
-https.createServer({
-  // SNICallback let's us get the correct cert
-  // depening on what the domain the user asks for
-  SNICallback: (domain, callback) => callback(
-    certs[domain] ? null : new Error('No such cert'),
-    certs[domain] ? certs[domain].secureContext : null
-  ),
-  // But we still have the server with a "default" cert
-  key: certs['olafrick.se'].key,
-  cert: certs['olafrick.se'].cert
-},(req,res) => {
+https
+  .createServer(
+    {
+      // SNICallback lets us get the correct certs
+      // depending on what the domain the user asks for
+      SNICallback: (domain, callback) =>
+        callback(
+          certs[domain] ? null : new Error("No such cert"),
+          certs[domain] ? certs[domain].secureContext : null
+        ),
+      // But we still have the server start with a "default" cert
+      key: certs["olafrick.se"].key,
+      cert: certs["olafrick.se"].cert
+    },
+    (req, res) => {
+      // replace setResponseHeaders
+      setResponseHeaders(req, res);
 
-  // Set/replace response headers
-  setResponseHeaders(req,res);
+      const host = req.headers.host;
 
-  // Routing
-  let host = req.headers.host,
-      url = req.url,
-      portToUse;
+      let url = req.url;
+      let portToUse;
 
-  url += (url.substr(-1) != '/' ? '/' : '');
+      url = url + (url.substr(-1) != "/" ? "/" : "");
 
-  for(let route in routes){
-    let port = routes[route];
-    if(route.includes('/')){
-      route += (route.substr(-1) != '/' ? '/' : '')
+      for (let route in routes) {
+        let port = routes[route];
+
+        if (route.includes("/")) {
+          route += route.substr(-1) != "/" ? "/" : "";
+        }
+
+        if (route === host) {
+          portToUse = port;
+        } else if (url !== "/" && route.indexOf(host + url) === 0) {
+          portToUse = port;
+        }
+      }
+
+      if (portToUse && portToUse.redirect) {
+        // redirect to domain without www
+        let urlLocation = "https://" + portToUse.redirect;
+        res.writeHead(301, { Location: urlLocation });
+        res.end();
+      } else if (portToUse) {
+        proxy.web(req, res, { target: "http://127.0.0.1:" + portToUse });
+      } else {
+        res.statusCode = 404;
+        res.end("This is not the page you are looking for.");
+      }
     }
-    if(route == host){
-      portToUse = port;
-    }
-    else if (url != '/' && (host + url).indexOf(route) == 0){
-      portToUse = port;
-    }
-  }
+  )
+  .listen(443);
 
-  // Redirects
-  if(portToUse && portToUse.redirect){
-    let url = 'https://' + portToUse.redirect;
-    res.writeHead(301, {'Location': url});
-    res.end();
-  }
-
-  // Serve the correct app for a domain
-  else if (portToUse){
-    proxy.web(req,res,{target:'http://127.0.0.1:' + portToUse});
-  }
-  else {
-    res.statusCode = 404;
-    res.end('No such url!');
-  }
-
-}).listen(443);
-
-
-function setResponseHeaders(req,res){
-
-  // there is a built in node function called res.writeHead
-  // that writes http response headers
-  // store that function in another property
+function setResponseHeaders(req, res) {
   res.oldWriteHead = res.writeHead;
+  res.writeHead = function(statusCode, headers) {
+    res.setHeader("x-powered-by", "vgl server");
 
-  // and then replace it with our function
-  res.writeHead = function(statusCode, headers){
+    // Security related
+    res.setHeader(
+      "strict-transport-security",
+      "max-age=31536000; includeSubDomains; preload"
+    );
+    res.setHeader("x-frame-options", "SAMEORIGIN");
+    res.setHeader("x-xss-protection", "1");
+    res.setHeader("x-content-type-options", "nosniff");
+    res.setHeader(
+      "content-security-policy",
+      "default-src * 'unsafe-inline' 'unsafe-eval'"
+    );
 
-    // set/replace our own headers
-    res.setHeader('x-powered-by','Powered by Ola');
-
-    // security related
-    res.setHeader('strict-transport-security','max-age=31536000; includeSubDomains; preload');
-    res.setHeader('x-frame-options','SAMEORIGIN');
-    res.setHeader('x-xss-protection', '1');
-    res.setHeader('x-content-type-options','nosniff');
-    res.setHeader('content-security-policy',"default-src * 'unsafe-inline' 'unsafe-eval'");
-
-    // call the original write head function as well
-    res.oldWriteHead(statusCode,headers);
-  }
-
+    // Call the original write head function as well
+    res.oldWriteHead(statusCode, headers);
+  };
 }
 
-function readCerts(pathToCerts){
-
+function readCerts(pathToCerts) {
   let certs = {},
-      domains = fs.readdirSync(pathToCerts);
+    domains = fs.readdirSync(pathToCerts);
 
   // Read all ssl certs into memory from file
-  for(let domain of domains){
-    let domainName = domain.split('-0')[0];
+  for (let domain of domains) {
+    let domainName = domain.split("-0")[0];
     certs[domainName] = {
-      key:  fs.readFileSync(path.join(pathToCerts,domain,'privkey.pem')),
-      cert: fs.readFileSync(path.join(pathToCerts,domain,'fullchain.pem'))
+      key: fs.readFileSync(path.join(pathToCerts, domain, "privkey.pem")),
+      cert: fs.readFileSync(path.join(pathToCerts, domain, "fullchain.pem"))
     };
-    certs[domainName].secureContext = tls.createSecureContext(certs[domainName]);
+    certs[domainName].secureContext = tls.createSecureContext(
+      certs[domainName]
+    );
   }
-
   return certs;
-
 }
 
-function renewCerts(){
-
-  exec('certbot renew',(error,stdOut,stdError)=>{
-    console.log('renewing certs',stdOut);
-    certs = readCerts('/etc/letsencrypt/live');
+function renewCerts() {
+  exec("certbot renew", (error, stdout, stdError) => {
+    console.log("renewing certs", stdout);
+    certs = readCerts("/etc/letsencrypt/live");
   });
-
 }
 
 // Renew certs if needed on start
